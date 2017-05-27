51777bb222fbb49d1813c14964c8e7c3f3b2e1ed
Wayne <waynejo1024@gmail.com>
2017-05-28 00:50:20 +0900

    add nested IF statement

--- demo/src/test/scala/LuaDemoExprTest.scala
+++ demo/src/test/scala/LuaDemoExprTest.scala
@@ -1,6 +1,7 @@
 import expr.element._
 import org.scalatest._
 import expr._
+import expr.SDExprJust
 import expr.element.core.SDValue
 
 class LuaDemoExprTest extends FunSuite {
@@ -13,6 +14,38 @@ class LuaDemoExprTest extends FunSuite {
         ).asInstanceOf[SDValue]) ==
             """if true and true then
               |    true
+              |end
+              |""".stripMargin)
+    }
+
+    test("if else statement") {
+        assert(LuaPrinter.print(Converter.convert(
+            if (bool(true) && bool(true)) {
+                SDExprJust(bool(true))
+            } else {
+                SDExprJust(bool(false))
+            }
+        ).asInstanceOf[SDValue]) ==
+            """if true and true then
+              |    true
+              |else
+              |    false
+              |end
+              |""".stripMargin)
+    }
+
+    test("nested if statement") {
+        assert(LuaPrinter.print(Converter.convert(
+            if (bool(true) && bool(true)) {
+                if (bool(false)) {
+                    SDExprJust(bool(true))
+                }
+            }
+        ).asInstanceOf[SDValue]) ==
+            """if true and true then
+              |    if false then
+              |        true
+              |    end
               |end
               |""".stripMargin)
     }
--- library/src/main/scala/Converter.scala
+++ library/src/main/scala/Converter.scala
@@ -8,17 +8,23 @@ object Converter {
     def convertMacro[T: c.WeakTypeTag](c: Context)(code: c.Expr[Any]): c.Expr[Any] = {
         import c.universe._
 
+        def convertCode(code: c.universe.Tree): c.universe.Tree = {
+            code match {
+                case If(cond, thenp, Literal(Constant(_))) => q"SDExprIF(${removeImplicit(cond)}, ${convertCode(thenp)})"
+                case If(cond, thenp, elsep) => q"SDExprIF(${removeImplicit(cond)}, ${convertCode(thenp)}, ${convertCode(elsep)})"
+                case expr => expr
+            }
+        }
+
         def removeImplicit(value: c.universe.Tree): c.universe.Tree = {
             value match {
-                case Apply(Select(_, TermName("BoolValue2bool")), List(tree)) => tree
+                case Apply(Select(_, TermName("BoolValue2bool")), List(tree)) => convertCode(tree)
                 case tree => tree
             }
         }
 
         val result = code match {
-            case Expr(If(cond, thenp, Literal(Constant(_)))) => q"SDExprIF(${removeImplicit(cond)}, $thenp)"
-            case Expr(If(cond, thenp, elsep)) => q"SDExprIF(${removeImplicit(cond)}, $thenp, $elsep)"
-            case Expr(expr) => expr
+            case Expr(expr) => convertCode(expr)
         }
         c.Expr(result)
     }
